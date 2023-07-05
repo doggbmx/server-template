1885834198cb538f8a2a3ad7fec8ab6f74c5b85e
josemlegal <josemlegal97@gmail.com>
2023-07-05 11:46:41 -0400

    Change the user queries and user serializer

--- src/feature/user/data/query_scripts/queries.ts
+++ src/feature/user/data/query_scripts/queries.ts
@@ -5,13 +5,13 @@ export const SELECT_USER_QUERY = `SELECT * FROM public.user WHERE id = $1;`;
 export const SELECT_USER_BY_EMAIL = `SELECT * FROM public.user WHERE email = $1 LIMIT 1;`;
 
 export const INSERT_USER_QUERY = `
-INSERT INTO public.user (username, email, password)
+INSERT INTO public.user (name, email, password)
 VALUES ($1, $2, $3)
 RETURNING *;
 `;
 
 export const UPDATE_USER_QUERY = `
 UPDATE public.user
-SET username=$2, email=$3, password=$4, recovery_token=$5
+SET name=$2, email=$3, password=$4, recovery_token=$5
 WHERE id=$1 RETURNING *;
 `;
--- src/feature/user/data/utils/user_serializer.ts
+++ src/feature/user/data/utils/user_serializer.ts
@@ -3,7 +3,7 @@ import { User } from "../../domain/models/user_model";
 export const userFromPG = (item: any): User => {
   return {
     userId: item.id,
-    name: item.username,
+    name: item.name,
     email: item.email,
     password: item.password,
     recoveryToken: item.recovery_token,
