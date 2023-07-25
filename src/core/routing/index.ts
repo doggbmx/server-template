7e3c56542245dc1510268fb8866db241cbb57b1e
doggbmx <manuelorticosta@gmail.com>
2023-07-25 11:05:31 -0400

    adds authHandler to routing

--- src/core/routing/index.ts
+++ src/core/routing/index.ts
@@ -1,10 +1,12 @@
 import express, { Application } from "express";
 import { usersRouter } from "../../feature/user/presentation";
 import { authRouter } from "../../feature/auth/presentation/auth_router";
+import { authHandler } from "../middlewares/auth_handler";
 
 export const configureRouting = (app: Application) => {
   const router = express.Router();
   app.use("/api", router);
   router.use("/auth", authRouter);
+  router.use(authHandler);
   router.use("/users", usersRouter);
 };
